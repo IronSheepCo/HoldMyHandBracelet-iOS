1790e3a73930a798ffa96c05e1eeab5eefdeaac6
Radu Dita <hades2510@yahoo.com>
2016-11-08 18:40:47 +0200

    added delegate for BluetoothManager

---
+++ HMH/BluetoothManagerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  BluetoothManagerDelegate.swift
+//  HMH
+//
+//  Created by Mac on 08/11/2016.
+//  Copyright © 2016 ironsheep.tech. All rights reserved.
+//
+
+import Foundation
+
+protocol BluetoothManagerDelegate: class{
+    func closestBeacon(_ beacon: HMHBeacon);
+}
