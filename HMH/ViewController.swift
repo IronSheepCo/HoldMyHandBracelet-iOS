62e171b422ae4856ba3f1a7ad96d5136bc2dc81c
Radu Dita <hades2510@yahoo.com>
2016-11-10 18:07:54 +0200

    fixed wrong edge

--- HMH/ViewController.swift
+++ HMH/ViewController.swift
@@ -60,7 +60,7 @@ class ViewController: UIViewController, BluetoothManagerDelegate {
         }
         
         //create edges
-        let edges = [ [0, 1], [1,2], [2,3], [3,0], [0,4], [4,5], [4,6], [4,7], [7,8], [7,9], [9,10] ]
+        let edges = [ [0, 1], [1,2], [2,3], [3,0], [0,4], [4,5], [4,6], [4,7], [7,8], [7,9], [8,10] ]
         
         for edge in edges
         {
