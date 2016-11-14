b6922167deeca8231f1f7fcd7908b9ac18a4b9ef
Radu Dita <hades2510@yahoo.com>
2016-11-14 17:16:53 +0200

    added config entries for timeouts

--- HMH/Config.swift
+++ HMH/Config.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 class Config{
+    static let NON_EDGE_JUMP_MILLI = 2000
+    static let CHANGE_NODE_MILLI = 1000
+    
     static let CURRENT_NODE_DEDUCTION = 0.5
     static let NEIGHBOUR_NODE_DEDUCTION = 0.35
     static let BEACON_MIN_DISTANCE = 0.1
