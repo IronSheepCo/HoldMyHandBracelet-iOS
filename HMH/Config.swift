de9ed144e90ff11885517ccbad09fdd08ac7e07a
Radu Dita <hades2510@yahoo.com>
2016-11-14 16:49:51 +0200

    added config class

---
+++ HMH/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  HMH
+//
+//  Created by Mac on 14/11/2016.
+//  Copyright © 2016 ironsheep.tech. All rights reserved.
+//
+
+import Foundation
+
+class Config{
+    static let CURRENT_NODE_DEDUCTION = 0.5
+    static let NEIGHBOUR_NODE_DEDUCTION = 0.35
+    static let BEACON_MIN_DISTANCE = 0.1
+}
